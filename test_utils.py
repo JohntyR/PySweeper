9b0003e3fa8a2c854ef8c194af96f1c8d9398e93
John Ronis <jronis@gmail.com>
2019-10-31 14:36:57 +0200

    refactored utils, adjusted tests

--- test_utils.py
+++ test_utils.py
@@ -2,6 +2,7 @@
 import unittest
 import pygame as pg
 import utils
+from tile import Tile
 
 
 class TestUtils(unittest.TestCase):
@@ -15,38 +16,20 @@ class TestUtils(unittest.TestCase):
         """quit the initiated pygame"""
         # pg.quit()
 
-    def test_initialise_game(self):
+    def test_init_game(self):
         """Check caption has been set correctly"""
-        utils.initialise_game()
+        utils.init_game()
         self.assertEqual(pg.display.get_caption()[0], utils.NAME)
 
-    def test_create_screen(self):
-        """Check if screen has been created"""
-        utils.create_screen()
-        self.assertIsNotNone(pg.display.get_surface())
-
-    # TODO: can be removed - moved to const
-    def test_return_mine_count(self):
-        """Test if mine count is correct"""
-        mine_count = utils.return_mine_count()
-        self.assertEqual(mine_count, utils.MINE_COUNT)
-
-    def test_generate_text(self):
-        """Test if font object is created"""
-        pg.font.init()
-        font_surface = utils.generate_text(100, 200)
-        self.assertIsInstance(font_surface, pg.Surface)
-
-    def test_current_coordinates(self):
-        """test if tuple of coords is created"""
-        pg.font.init()
-        curr_coords = utils.current_coordinates(100, 200)
-        self.assertIsInstance(curr_coords, tuple)
-
     def test_generate_tiles(self):
-        """test if list of appropriate length is returned """
+        """Test if list of appropriate length is returned """
         tile_list = utils.generate_tiles()
         self.assertEqual(len(tile_list), utils.TILE_COUNT)
+
+    def test_generate_tiles_2(self):
+        """Test if list contains Tile objects"""
+        tile_list = utils.generate_tiles()
+        self.assertIsInstance(tile_list[0], Tile)
 
     def test_generate_mine_sequence(self):
         """test if a list of appropriate length is returned"""
@@ -56,14 +39,14 @@ class TestUtils(unittest.TestCase):
     def test_game_over(self):
         """Test if font object created"""
         pg.font.init()
-        font_surface = utils.game_over()
+        font_surface = utils.game_over(True)
         self.assertIsInstance(font_surface, pg.Surface)
 
     def test_game_over_coordinates(self):
         """test if rect instance is returned"""
         pg.font.init()
         font = utils.FONT.render("test", True, utils.FONT_COLOUR)
-        game_over_coords = utils.game_over_coordinates(font)
+        game_over_coords = utils.game_over_coords(font)
         self.assertIsInstance(game_over_coords, pg.Rect)
 
     def test_adjacent_bomb_count(self):
@@ -99,29 +82,23 @@ class TestUtils(unittest.TestCase):
         ]
         self.assertEqual(adj_list, adj_list_2)
 
-    def test_generate_mine_count_text(self):
+    def test_generate_mine_text(self):
         """Test if font object created"""
         pg.font.init()
-        font_surface = utils.generate_mine_count_text(1)
+        font_surface = utils.generate_mine_text(1)
         self.assertIsInstance(font_surface, pg.Surface)
 
     def test_mine_count_text_coordinates(self):
         """Test if Rect instance is returned"""
         pg.font.init()
         font = utils.FONT.render("test", True, utils.FONT_COLOUR)
-        mine_count_text_coords = utils.mine_count_text_coordinates(font, (10, 10))
-        self.assertIsInstance(mine_count_text_coords, pg.Rect)
-
-    def test_generate_mines_left_text(self):
-        """Test if font object created"""
-        pg.font.init()
-        font_surface = utils.generate_mines_left_text(1)
-        self.assertIsInstance(font_surface, pg.Surface)
+        mine_count_text_coords = utils.mine_count_coords(font, (10, 10))
+        self.assertIsInstance(mine_count_text_coords, tuple)
 
     def test_mines_left_coordinates(self):
         """Test if coord tuple is returned"""
         pg.font.init()
-        mines_coords = utils.mines_left_coordinates(1)
+        mines_coords = utils.mines_left_coords(1)
         self.assertIsInstance(mines_coords, tuple)
 
 
--- utils.py
+++ utils.py
@@ -5,7 +5,6 @@ from tile import Tile
 
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 360
-FONT_COLOUR = (255, 255, 255)
 DIFFICULTY = (10, 10)
 TILE_COUNT = DIFFICULTY[0] * DIFFICULTY[1]
 MINE_COUNT = (TILE_COUNT) // 5
@@ -17,13 +16,15 @@ ADJ_LIST = [-11, -10, -9, -1, 1, 9, 10, 11]
 
 pg.font.init()
 
-# set font
+# set fonts
 FONT = pg.font.SysFont("TAHOMA", 12)
+FONT_COLOUR = (255, 255, 255)
 GAME_OVER_FONT = pg.font.SysFont("TAHOMA", 36)
+GAME_OVER_FONT_COLOUR = (255, 0, 0)
 
 
-def initialise_game():
-    """start up neccessary pg items"""
+def init_game():
+    """start up neccessary pg items and return a screen."""
     # load program logo image
     logo = pg.image.load(ICON)
 
@@ -31,31 +32,7 @@ def initialise_game():
     pg.display.set_icon(logo)
     pg.display.set_caption(NAME)
 
-
-def create_screen():
-    """uses window width and height constants to create a surface"""
     return pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-
-# TODO: can be removed - replaced by const
-def return_mine_count():
-    """number of starting mines"""
-    return MINE_COUNT
-
-
-# TODO: not needed anymore - no longer displaying coord text
-def generate_text(x_pos, y_pos):
-    """creates new surface with text stating the current co-ords"""
-    return FONT.render(f"x: {x_pos} y: {y_pos}", True, FONT_COLOUR)
-
-
-# TODO: not needed anymore  - no longer displaying coord text
-def current_coordinates(x_pos, y_pos):
-    """returns co-ordinates needed to fit text in bottom corner"""
-    return (
-        (WINDOW_WIDTH - FONT.size(f"x: {x_pos} y: {y_pos}")[0]) - 5,
-        (WINDOW_HEIGHT - FONT.size(f"x: {x_pos} y: {y_pos}")[1]) - 5,
-    )
 
 
 def generate_tiles():
@@ -72,14 +49,17 @@ def generate_mine_sequence():
     return random.sample(range(TILE_COUNT), (MINE_COUNT))
 
 
-# TODO: refactor to include a "you won"
-def game_over():
+def game_over(lost):
     """Display text that game is finished"""
-    return GAME_OVER_FONT.render("GAME OVER", True, (255, 0, 0))
+    if lost:
+        text = "GAME OVER!"
+    else:
+        text = "YOU WON!"
+
+    return GAME_OVER_FONT.render(text, True, GAME_OVER_FONT_COLOUR)
 
 
-# TODO: coord refactor
-def game_over_coordinates(font):
+def game_over_coords(font):
     """return coords to position game over text"""
     return font.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
@@ -100,27 +80,20 @@ def adjacent_bomb_count(tile_i):
     return new_adjacent_tiles
 
 
-# TODO: merge into one iwth gerneate_mines_left_text
-def generate_mine_count_text(mine_count):
-    """creates new surface with text stating the number of mines"""
-    if mine_count == 0:
-        mine_count = ""
-    return FONT.render(f"{mine_count}", True, FONT_COLOUR)
+def generate_mine_text(mines):
+    """create new surface with text"""
+    if mines == 0:
+        mines = ""
+    return FONT.render(f"{mines}", True, FONT_COLOUR)
 
 
-# TODO: refactor coords
-def mine_count_text_coordinates(font, tile_mids):
+def mine_count_coords(font, tile_mids):
     """returns co-ordinates needed to fit text in bottom corner"""
-    return font.get_rect(center=(tile_mids[0], tile_mids[1]))
+    font_obj = font.get_rect(center=(tile_mids[0], tile_mids[1]))
+    return (font_obj[0], font_obj[1])
 
 
-# TODO: merge with generate_mine_count_text
-def generate_mines_left_text(mines_left):
-    """creates new surface with text stating the current co-ords"""
-    return FONT.render(f"{mines_left}", True, FONT_COLOUR)
-
-
-def mines_left_coordinates(mines_left):
+def mines_left_coords(mines_left):
     """returns co-ordinates needed to fit text in bottom corner"""
     return (
         (WINDOW_WIDTH - FONT.size(f"{mines_left}")[0]) - 5,
