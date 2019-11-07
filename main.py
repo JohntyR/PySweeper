5706902bf51362cfc0635b738e75540427ab6a37
John Ronis <jronis@gmail.com>
2019-11-07 15:17:41 +0200

    comment clarification

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def main():
                                 for adj in adj_array:
                                     if tile_set[adj].is_mine:
                                         mine_count += 1
-                                # If there are no mines, add all adjacent tiles to be clicked
+                                # If there are no mines, add all adjacent tiles to tiles_to_be_clicked
                                 if mine_count == 0:
                                     for adj in adj_array:
                                         if adj not in tiles_to_be_clicked:
@@ -134,9 +134,9 @@ def main():
             else:
                 screen.blit(tile.img, (tile.x_pos, tile.y_pos))
 
-        # Losing condition: a mine has been clicked - create and blit text, stop timer
+        # Losing condition: a mine has been clicked - reveal all tiles, create and blit text, stop timer
         if any((tile.is_mine and tile.is_clicked) for tile in tile_set):
-            # Click all tiles
+            # Reveal all tiles
             for i, tile2 in enumerate(tile_set):
                 adj_array = ut.adjacent_bomb_count(i)
 
@@ -146,7 +146,7 @@ def main():
                         mine_count += 1
 
                 tile2.clicked(mine_count)
-
+            # Create and blit game over text, stop timer
             game_over_text = ut.game_over(True)
             game_over_coords = ut.game_over_coords(game_over_text)
             screen.blit(game_over_text, game_over_coords)
